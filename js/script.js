dd27c1b0538edff4643a305f303f6dff2b80f810
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-01-31 19:26:28 +0530

    nav

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@ document.querySelectorAll('.has-submenu > a').forEach(function(item) {
     item.addEventListener('click', function(e) {
         e.preventDefault();  // Prevent default anchor behavior
 
-        var submenu = this.nextElementSibling; // Get the submenu
+        // Get the submenu related to this clicked item
+        var submenu = this.nextElementSibling; 
 
-        // Toggle the 'active' class to show or hide the submenu
+        // Toggle the 'active' class on this submenu only
         submenu.classList.toggle('active');
+
+        // Close other submenus if they are open
+        document.querySelectorAll('.has-submenu > ul.submenu').forEach(function(otherSubmenu) {
+            if (otherSubmenu !== submenu) {
+                otherSubmenu.classList.remove('active');
+            }
+        });
     });
 });
