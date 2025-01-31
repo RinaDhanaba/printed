5d1b9500dce746adec1e55118f2ec28ec8aa8ff3
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-01-31 16:49:26 +0530

    home

--- app/topbar/cupon-topbar.php
+++ app/topbar/cupon-topbar.php
@@ -1,4 +1,4 @@
 <div class="top-bar">
-    <p><strong>10% off your first order.*</strong> <span class="small-text">*T&Cs apply.</span></p>
+    <div class="center"><p><strong>10% off your first order.*</strong><span class="small-text">*T&Cs apply.</span></p></div>
     <button class="promo-code">Use code: <strong>WELCOME10</strong></button>
 </div>
