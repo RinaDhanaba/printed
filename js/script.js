0df923970858bc75faa5ba057c29cc9c97c10070
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-01-31 19:15:23 +0530

    nav

--- js/script.js
+++ js/script.js
@@ -9,23 +9,3 @@ document.querySelectorAll('.has-submenu > a').forEach(function(item) {
         submenu.classList.toggle('active');
     });
 });
-
-
-
-document.getElementById('hamburger-btn').addEventListener('click', function() {
-    var navMenu = document.getElementById('nav-menu');
-    navMenu.classList.toggle('active');
-});
-
-var submenuItems = document.querySelectorAll('.has-submenu > a');
-submenuItems.forEach(function(item) {
-    item.addEventListener('click', function(e) {
-        var submenu = this.nextElementSibling;
-        if (submenu.style.display === "block") {
-            submenu.style.display = "none";
-        } else {
-            submenu.style.display = "block";
-        }
-        e.preventDefault(); // Prevent default anchor behavior
-    });
-});
