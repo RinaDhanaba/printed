22c327722e0524b9cb806140bfe85424e0082ac2
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-01-31 19:13:48 +0530

    menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// Add click event listener for menu items with submenus
+document.querySelectorAll('.has-submenu > a').forEach(function(item) {
+    item.addEventListener('click', function(e) {
+        e.preventDefault();  // Prevent default anchor behavior
+
+        var submenu = this.nextElementSibling; // Get the submenu
+
+        // Toggle the 'active' class to show or hide the submenu
+        submenu.classList.toggle('active');
+    });
+});
+
+
+
 document.getElementById('hamburger-btn').addEventListener('click', function() {
     var navMenu = document.getElementById('nav-menu');
     navMenu.classList.toggle('active');
