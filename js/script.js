4b4791cbbc249a6308f705f8aab4f9b0ed0aa551
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-01-31 18:50:24 +0530

    nav

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
+// Hamburger menu toggle
 document.getElementById('hamburger-btn').addEventListener('click', function() {
     var menu = document.getElementById('nav-menu');
     menu.classList.toggle('show');
 });
 
+// Submenu toggle
 document.querySelectorAll('.has-submenu > a').forEach(function(item) {
     item.addEventListener('click', function(e) {
         var parent = item.parentElement;
-        parent.classList.toggle('open');
+        parent.classList.toggle('open');  // Toggle open state for the submenu
         e.preventDefault(); // Prevent link from redirecting
     });
 });
