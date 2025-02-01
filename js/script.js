0b22e734ae0d5a3f5fe7f601f2d0aa4a25533c13
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-01 22:25:08 +0530

    menu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const menu = document.querySelector(".menu");
+    const closeBtn = document.createElement("div");
 
-    menuToggle.addEventListener("click", function() {
-        menu.classList.toggle("active");
+    // Create a close button inside the menu
+    closeBtn.classList.add("close-menu");
+    closeBtn.innerHTML = "&times;";
+    menu.prepend(closeBtn);
+
+    menuToggle.addEventListener("click", function () {
+        menu.classList.add("active");
+    });
+
+    closeBtn.addEventListener("click", function () {
+        menu.classList.remove("active");
     });
 
     // Mobile Submenu Toggle
     const submenus = document.querySelectorAll(".has-submenu > a");
-    submenus.forEach(submenu => {
-        submenu.addEventListener("click", function(event) {
+    submenus.forEach((submenu) => {
+        submenu.addEventListener("click", function (event) {
             event.preventDefault();
             this.nextElementSibling.classList.toggle("active");
         });
