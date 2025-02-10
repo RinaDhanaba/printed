d217edaaff780814060bca08b13942333716014e
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-10 17:32:18 +0530

    sidebar

--- sidebar-elements.php
+++ sidebar-elements.php
@@ -1,9 +1,10 @@
 <!-- Include the header -->
 <?php include('app/layout-templete/header.php'); ?>
 
+<div class="container" >
 <!-- Sidebar Section -->
 <?php include 'app/layout-templete/sidebar.php'; ?>
-
+</div>
 
 <!-- Blog section -->
 <?php include('app/layout-templete/trending_blog.php'); ?>
