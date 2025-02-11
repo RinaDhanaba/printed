2b79672c17664c556e1ce990e4d55aa3f05e3ca4
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-11 10:49:59 +0530

    res cta

--- basket.php
+++ basket.php
@@ -12,7 +12,7 @@
 <section id="basket-cta">
     <div class="container">
     <img src="./media/req-18013-bespoke-widget-product-pages-288x140.webp"/>
-    <div class="cateory-cta-content">
+    <div class="cta-content">
         <h2>For something extraordinary try our Bespoke Service</h2>
         <p>We know that print isn’t one size fits all. No matter the shape, size or scope of the project we’ll help you get your print, your way.</p>
     </div>
@@ -23,14 +23,30 @@
 <style>
     #basket-cta{
         background: var(--gary-bg);
-        margin-bottom:100px;
+        margin-bottom:50px;
     }
     #basket-cta .container{display: flex;
     gap: 15px;
     justify-content: center;
-    align-items: center;
-    margin-bottom:-30px;
+    align-items: center;    
+    padding-top: 30px;
+    position: relative;
+    bottom: -20px;
     }
+    @media (max-width:500px){
+        #basket-cta .container{
+            flex-direction:column;
+        } 
+    }
+
+    #basket-cta .cta-content{
+        flex: 1;
+    }
+
+    #basket-cta img{
+        max-width: 270px;
+    }
+    #basket-cta h2{margin-bottom:15px}
 </style>
 
 <!-- Include the footer -->
