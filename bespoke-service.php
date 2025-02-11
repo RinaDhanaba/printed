dc6a5ba9098b661498f8921a86894978063a86df
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-11 12:39:15 +0530

    page stucture

---
+++ bespoke-service.php
@@ -0,0 +1,13 @@
+<!-- Include the header -->
+<?php include('app/layout-templete/header.php'); ?>
+
+
+
+<!-- blog section  -->
+<?php include('app/layout-templete/trending_blog.php'); ?>
+
+<!-- trustpilot section -->
+<?php include('/app/layout-templete/trustpilot.php'); ?>
+
+<!-- Include the footer -->
+<?php include('app/layout-templete/footer.php'); ?>
---
+++ contact-us.php
@@ -0,0 +1,10 @@
+<!-- Include the header -->
+<?php include('app/layout-templete/header.php'); ?>
+
+
+
+<!-- trustpilot section -->
+<?php include('/app/layout-templete/trustpilot.php'); ?>
+
+<!-- Include the footer -->
+<?php include('app/layout-templete/footer.php'); ?>
