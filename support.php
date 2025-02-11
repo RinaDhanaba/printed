cf4cc7520bd42423369bed72eb645fac81e3e7ce
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-11 16:07:09 +0530

    slider

--- support.php
+++ support.php
@@ -237,7 +237,7 @@ function openTab(evt, tabId) {
         prevButton.style.display = 'none';
         nextButton.style.display = 'none';
     } else {
-        prevButton.style.display = currentIndex === -1 ? 'none' : 'block';
+        prevButton.style.display = currentIndex === 0 ? 'none' : 'block';
         nextButton.style.display = currentIndex >= tools.length - Math.floor(visibleAreaWidth / toolWidth) ? 'none' : 'block';
     }
 }
