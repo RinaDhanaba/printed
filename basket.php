09bcffbed47a95f3e81938dc8339009fe68156e9
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-11 10:33:39 +0530

    cta

--- basket.php
+++ basket.php
@@ -9,5 +9,14 @@
 <!-- Blog section -->
 <?php include('app/layout-templete/trending_blog.php'); ?>
 
+<section id="cateory-cta">
+    <img src="./media/req-18013-bespoke-widget-product-pages-288x140.webp"/>
+    <div class="cateory-cta-content">
+        <h2>For something extraordinary try our Bespoke Service</h2>
+        <p>We know that print isn’t one size fits all. No matter the shape, size or scope of the project we’ll help you get your print, your way.</p>
+    </div>
+    <a href="#"> Get a quote</a>
+</section>
+
 <!-- Include the footer -->
 <?php include('app/layout-templete/footer.php'); ?>
