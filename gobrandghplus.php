2deece1f7dccb378f04a14bef8a1c3f43295795a
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-12 10:16:03 +0530

    plus

--- gobrandghplus.php
+++ gobrandghplus.php
@@ -1,7 +1,37 @@
 <!-- Include the header -->
 <?php include('app/layout-templete/header.php'); ?>
 
+<div class="background-section"></div>
 
+
+<section class="container" style="text-align: center;margin-bottom:50px;max-width: 450px;margin: 20px auto 50px;">
+    <h1>A print account tailored to you.</h1>
+    <p>Printed Plus is our premium service designed to make managing print easier and more cost effective for customers with high volume print requirements.
+    </p>
+    <p>From a dedicated Account Manager to a choice of discounts, free artwork checks and a wide range of other services, Printed Plus has been set up to support you with everything you need for your print.</p>
+</section>
+
+<section class="flex" style="background:var(--gary-bg);">
+    <img src="./media/category_custom_shape_1.jpg" alt="">
+    <div class="section_content">
+        <h2>Who is Printed Plus for?</h2>
+        <p>Printed Plus is a free service available to all customers who spend over £5,000 on print each year. </p>
+        <p>If you're interested in joining Printed Plus please contact the team at 
+        Printedplus@printed.com. We can discuss how we can support all of your print requirements.</p>
+    </div>
+</section>
+
+<div class="background-section"  style="background-image: url('./media/cr-1225-printed-plus-webpage-feedback-unleash-the-power-2000250-1-copy.webp');"></div>
+
+
+
+
+
+
+<section style="background:var(--gary-bg); padding:30px 5px;">
+    <strong>If you're interested in joining Printed Plus please get in touch with the</strong>
+    <strong>team on Printedplus@printed.com and a member of the team will get in touch.</strong>
+</section>
 
 <!-- trustpilot section -->
 <?php include('app/layout-templete/trustpilot.php'); ?>
