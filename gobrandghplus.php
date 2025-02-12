56660b1059ecb28f094d081ef8f1291e08758b6d
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-12 10:22:43 +0530

    pluse

--- gobrandghplus.php
+++ gobrandghplus.php
@@ -4,20 +4,22 @@
 <div class="background-section"></div>
 
 
-<section class="container" style="text-align: center;margin-bottom:50px;max-width: 450px;margin: 20px auto 50px;">
-    <h1>A print account tailored to you.</h1>
+<section class="container" style="text-align: center;margin-bottom:50px;max-width: 500px;margin: 80px auto; text-wrap:balance;">
+    <h1><strong>A print account tailored to you.</strong></h1>
     <p>Printed Plus is our premium service designed to make managing print easier and more cost effective for customers with high volume print requirements.
     </p>
     <p>From a dedicated Account Manager to a choice of discounts, free artwork checks and a wide range of other services, Printed Plus has been set up to support you with everything you need for your print.</p>
 </section>
 
-<section class="flex" style="background:var(--gary-bg);">
+<section style="background:var(--gary-bg);">
+    <div class="container" style="display:flex; gap:40px;align-items: center;">
     <img src="./media/category_custom_shape_1.jpg" alt="">
-    <div class="section_content">
+    <div class="section_content" style="flex:1;">
         <h2>Who is Printed Plus for?</h2>
         <p>Printed Plus is a free service available to all customers who spend over £5,000 on print each year. </p>
         <p>If you're interested in joining Printed Plus please contact the team at 
         Printedplus@printed.com. We can discuss how we can support all of your print requirements.</p>
+    </div>
     </div>
 </section>
 
@@ -28,8 +30,8 @@
 
 
 
-<section style="background:var(--gary-bg); padding:30px 5px;">
-    <strong>If you're interested in joining Printed Plus please get in touch with the</strong>
+<section style="background:var(--gary-bg); padding:30px 5px; text-align:center;">
+    <strong>If you're interested in joining Printed Plus please get in touch with the</strong><br><br>
     <strong>team on Printedplus@printed.com and a member of the team will get in touch.</strong>
 </section>
 
